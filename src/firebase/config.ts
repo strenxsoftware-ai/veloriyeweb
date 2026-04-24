a0fe9c3e53b395020b3b279759b403c2b9612972
Strenx Software <strenxsoftware@gmail.com>
2026-04-24 12:05:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5869002534-263af",
+  "appId": "1:611404990471:web:53e6e0ae70fe14acba02a2",
+  "apiKey": "AIzaSyAj2AcJcFEAz5aDl7e1bQENDlYuVpkq8Js",
+  "authDomain": "studio-5869002534-263af.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "611404990471"
+};
